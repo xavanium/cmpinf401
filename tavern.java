1e4454617d4d36757b381cafbd8f3e500c774baa
Val <xavanium@gmail.com>
2026-09-05 15:31:39 +0000

    chipped away at it some more

--- tavern.java
+++ tavern.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public static void main(String[] args) {
     boolean isPubber = false;
-    double pint = 60, breadncheese = 120, custTart = 72, custTartDoz = 720, mealDeal = 168; //initializing menu item prices
-    String yn, yn2, pw = "j";
+    double pint = 60, breadncheese = 120, custTart = 72, custTartDoz = 720, mealDeal = 168; //initializing menu item prices in pence
+    String yn, yn2, pw = "j", psp = "j";
     Scanner k = new Scanner(System.in);
     System.out.println("Is there a customer to be waited on? (y/n)");
     yn = k.next();
@@ -22,30 +22,62 @@ public static void main(String[] args) {
             pw = k.next();
                 if (pw.equals("WEASEL")){
                     isPubber = true;
-                    i = 2;
                     System.out.println("Congrats, you're in! You will now receive discounted prices.");
-                }
-                else {
+                    break;
+                } else if (!(pw.equals("WEASEL"))&& i==0) {
                     System.out.println("Try again.");
-                    System.out.print("PASSWORD: ");
-                    pw = k.next();
-                        if (pw.equals("WEASEL")){
-                        isPubber = true;
-                        i = 2;
-                        System.out.println("Congrats, you're in! You will now receive discounted prices.");
-                } //TODO: clean this up somehow??
+                    continue;
+                } else {
+                    System.out.println("Sorry, you are not a pubber.");
                 }
-                if (!(pw.equals("WEASEL"))){
-                System.out.println("Sorry, you are NOT a pubber.");
-                break; //ends loop if customer doesnt get password
             }
             } 
-    }
     if(isPubber){ //applying discount prices for pubbers
         pint = 55;
         breadncheese = 9;
         custTartDoz = 660;
         mealDeal = 156;
     }
-    k.close(); //closing scanner to "prevent resource leaks"
+    System.out.println("Would you like to view the item prices in pence, shillings, or pounds?");
+    psp = k.next();
+        if (psp.equalsIgnoreCase("pence")) {
+            pMenu(pint, breadncheese, custTart, custTartDoz, mealDeal);
+        } else if (psp.equalsIgnoreCase("shillings")){
+            sMenu(pint, breadncheese, custTart, custTartDoz, mealDeal);
+        } else if (psp.equalsIgnoreCase("pounds")){
+            lbMenu(pint, breadncheese, custTart, custTartDoz, mealDeal);
+        } else { 
+            System.out.println("What? "); //TODO: fix this somehow idk
+        }
+    k.close();
+}
+
+public static void pMenu(double pint, double breadncheese, double custTart, double custTartDoz, double mealDeal){
+    System.out.println();
+    System.out.println("MENU: ");
+    System.out.println("PINT OF MEAD: " + pint + " PENCE");
+    System.out.println("BREAD AND CHEESE: " + breadncheese + " PENCE");
+    System.out.println("CUSTARD TART: " + custTart + " PENCE");
+    System.out.println("DOZEN OF CUSTARD TARTS: " + custTartDoz + " PENCE");
+    System.out.println("MEAL DEAL (Pint of mead + an order of bread and cheese): " + mealDeal + " PENCE");
+}
+
+public static void sMenu(double pint, double breadncheese, double custTart, double custTartDoz, double mealDeal){
+    System.out.println();
+    System.out.println("MENU: ");
+    System.out.println("PINT OF MEAD: " + pint/12 + " SHILLINGS");
+    System.out.println("BREAD AND CHEESE: " + breadncheese/12 + " SHILLINGS");
+    System.out.println("CUSTARD TART: " + custTart/12 + " SHILLINGS");
+    System.out.println("DOZEN OF CUSTARD TARTS: " + custTartDoz/12 + " SHILLINGS");
+    System.out.println("MEAL DEAL (Pint of mead + an order of bread and cheese): " + mealDeal/12 + " SHILLINGS");
+}
+
+public static void lbMenu(double pint, double breadncheese, double custTart, double custTartDoz, double mealDeal){
+    System.out.println();
+    System.out.println("MENU: ");
+    System.out.println("PINT OF MEAD: " + pint/240 + " POUNDS");
+    System.out.println("BREAD AND CHEESE: " + breadncheese/240 + " POUNDS");
+    System.out.println("CUSTARD TART: " + custTart/240 + " POUNDS");
+    System.out.println("DOZEN OF CUSTARD TARTS: " + custTartDoz/240 + " POUNDS");
+    System.out.println("MEAL DEAL (Pint of mead + an order of bread and cheese): " + mealDeal/240 + " POUNDS");
 }
