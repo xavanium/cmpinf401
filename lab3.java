e590aa69d222b01958c0b8bc112f2f887fc26dfa
Val <xavanium@gmail.com>
2026-09-14 20:27:23 +0000

    made it a little better but it needs more

--- lab3.java
+++ lab3.java
@@ -1,14 +1,26 @@
 /* Valerie Kuttler
-Lab 3: Dice Rolling
-CMPINF 401--SEC 1200 */
+LAB 3 - Dice Rolling
+CMPINF401 SEC 1200 */
+
 import java.util.*;
 public static void main(String[] args){
+    Scanner k = new Scanner(System.in);
+    String cont = "yes";
+    while (cont.equalsIgnoreCase("yes")){
+    System.out.print("Roll the dice this many times: ");
+    int timesRolled = k.nextInt();
     Random R = new Random();
-    int d1 = R.nextInt(6)+1;
-    int d2 = R.nextInt(6) + 1;
-    for (int i = 0; i<100; i++){
-        System.out.println(d1+d2);
-        d1 = R.nextInt(6) + 1;
-        d2 = R.nextInt(6)+1;
+    RollDice(timesRolled, R);
+    System.out.println("Would you like to continue? (yes/no)");
+    cont = k.next();
     }
+    k.close();
 }
+
+public static void RollDice(int timesRolled, Random R){
+        for (int i = 0; i < timesRolled; i++){
+            int d1 = R.nextInt(6) + 1;
+            int d2 = R.nextInt(6) + 1;
+            System.out.println(d1+d2);
+        }
+    }
