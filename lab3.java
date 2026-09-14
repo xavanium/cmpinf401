2fbed751c0799a8bad52590204d01ba20203c8cc
Val <xavanium@gmail.com>
2026-09-14 21:10:21 +0000

    i think i did it

--- lab3.java
+++ lab3.java
@@ -1,26 +1,34 @@
 /* Valerie Kuttler
 LAB 3 - Dice Rolling
-CMPINF401 SEC 1200 */
-
+CMPINF401 SEC1200 */
 import java.util.*;
-public static void main(String[] args){
+public static void main(String[] args) {
     Scanner k = new Scanner(System.in);
     String cont = "yes";
-    while (cont.equalsIgnoreCase("yes")){
-    System.out.print("Roll the dice this many times: ");
-    int timesRolled = k.nextInt();
+    int timesRolled;
     Random R = new Random();
-    RollDice(timesRolled, R);
-    System.out.println("Would you like to continue? (yes/no)");
-    cont = k.next();
+    while (cont.equalsIgnoreCase("yes")) {
+        System.out.print("Roll the dice this many times: ");
+        timesRolled = k.nextInt();
+        RollDice(timesRolled, R);
+        System.out.println("Would you like to continue? (yes/no)");
+        cont = k.next();
     }
     k.close();
 }
 
-public static void RollDice(int timesRolled, Random R){
-        for (int i = 0; i < timesRolled; i++){
-            int d1 = R.nextInt(6) + 1;
-            int d2 = R.nextInt(6) + 1;
-            System.out.println(d1+d2);
-        }
+public static void RollDice(int timesRolled, Random R) {
+    int[] counts = new int[13];
+    for (int i = 0; i < timesRolled; i++) {
+        int d1 = R.nextInt(6) + 1;
+        int d2 = R.nextInt(6) + 1;
+        int sum = d1 + d2;
+        System.out.println(sum);
+        counts[sum]++; // keep track of how many times each possible roll happened
     }
+    System.out.println("\nPercentage of each roll:");
+    for (int roll = 2; roll <= 12; roll++) {
+        double fraction = (double) counts[roll] / timesRolled;
+        System.out.println(roll + ": " + fraction*100+"%");
+    }
+}
